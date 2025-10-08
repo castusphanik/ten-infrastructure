cf0809044ec4194eb6df0d49281da7884116877a
castusphani@gmail.com <castusphani@gmail.com>
2025-10-08 21:06:56 +0530

    second push

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
 module "web_r53" {
   source          = "./modules/route53"
-  zone_id         = zone_id
+  zone_id         = var.zone_id
   record_name     = var.web_record_name
   target_dns_name = module.cloudfront.name
   target_zone_id  = module.cloudfront.zone_id
